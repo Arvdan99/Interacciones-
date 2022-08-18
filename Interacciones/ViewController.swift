5665a2d423ae19af5c67d27834e272790177ac29
Alumno <cesarrgomez@hotmail.com>
2022-08-18 07:15:45 -0700

    Initial Commit

---
+++ Interacciones/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Interacciones
+//
+//  Created by Alumno on 8/18/22.
+//  Copyright © 2022 Ulsa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
